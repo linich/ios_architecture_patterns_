4cf0845c1f92dd204e9d42beb40c9934e5f586c3
linich <linich.maksim@gmail.com>
2024-03-26 11:25:51 +0300

    Init HomeViewController from xib

--- ActivityList/ActivityList/SceneDelegate.swift
+++ ActivityList/ActivityList/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import ActivityListUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -19,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: scene)
-        window.rootViewController = ViewController()
+        window.rootViewController = HomeViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
---
+++ ActivityList/ActivityListUI/Home/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  ActivityListUI
+//
+//  Created by Maksim Linich on 26.03.24.
+//
+
+import UIKit
+
+public class HomeViewController: UIViewController {
+    public convenience init() {
+        self.init(nibName: "HomeViewController", bundle: Bundle(for: HomeViewController.self))
+    }
+}
