dac1486d5a85316e0dab8c35eb7fa844947e771e
linich <linich.maksim@gmail.com>
2024-04-01 11:20:24 +0300

    Add HomeServiceProtocol to the ActivityListDomain

---
+++ ActivityList/ActivityListDomain/Services/HomeService.swift
@@ -0,0 +1,21 @@
+//
+//  HomeService.swift
+//  ActivityListDomain
+//
+//  Created by Maksim Linich on 1.04.24.
+//
+
+
+public struct TasksListInfo {
+    let id: UUID
+    let name: String
+    let type: TasksListModel.TasksListType
+    let tasksCount: Int
+}
+
+public protocol HomeServiceProtocol {
+    typealias Result = Swift.Result<TasksListInfo, Error>
+    typealias Completion = (Result) -> Void
+    
+    func readTasksInfos(completion: Result) -> Void
+}
