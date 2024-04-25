36e886257cd56902456250d4b25981e1576c7d56
linich <linich.maksim@gmail.com>
2024-04-25 12:58:56 +0300

    Move ItemsView to production

---
+++ ActivityList/ActivityListUI/ItemsLists/ItemsView.swift
@@ -0,0 +1,66 @@
+//
+//  ItemsView.swift
+//  ActivityListUI
+//
+//  Created by Maksim Linich on 25.04.24.
+//
+
+import UIKit
+
+public class ItemsView: UIView {
+    public let emptyState = {
+        let view = UIView()
+        return view
+        
+    }()
+    
+    public let tableView = {
+        let tableView = UITableView()
+        tableView.register(ItemTableCellView.self, forCellReuseIdentifier: "\(ItemTableCellView.self)")
+        return tableView
+    }()
+    
+    public var items: [ItemData] = [] {
+        didSet {
+            dataSource.items = self.items
+            emptyState.isHidden = !self.items.isEmpty
+            tableView.isHidden = self.items.isEmpty
+        }
+    }
+    
+    private let dataSource = ItemsViewTableViewDataSource()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews();
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSubviews();
+    }
+
+    private func setupSubviews() {
+        tableView.dataSource = dataSource
+    }
+}
+
+internal class ItemsViewTableViewDataSource:NSObject, UITableViewDataSource {
+    public var items = [ItemData]()
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let tasksListCell = tableView.dequeueReusableCell(withIdentifier: "\(ItemTableCellView.self)", for: indexPath) as! ItemTableCellView
+        
+        let model = items[indexPath.row]
+        tasksListCell.titleLabel.text = model.title
+        tasksListCell.subtitleLabel.text = model.subtitle
+        tasksListCell.iconImageView.image = model.icon
+        
+        return tasksListCell
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+}
--- ActivityList/ActivityListUITests/Items/ItemsListViewTests.swift
+++ ActivityList/ActivityListUITests/Items/ItemsListViewTests.swift
@@ -9,64 +9,6 @@ import XCTest
 import UIKit
 import ActivityListUI
 
-class ItemsView: UIView {
-    public let emptyState = {
-        let view = UIView()
-        return view
-        
-    }()
-    
-    public let tableView = {
-        let tableView = UITableView()
-        tableView.register(ItemTableCellView.self, forCellReuseIdentifier: "\(ItemTableCellView.self)")
-        return tableView
-    }()
-    
-    public var items: [ItemData] = [] {
-        didSet {
-            dataSource.items = self.items
-            emptyState.isHidden = !self.items.isEmpty
-            tableView.isHidden = self.items.isEmpty
-        }
-    }
-    
-    private let dataSource = ItemsViewTableViewDataSource()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupSubviews();
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setupSubviews();
-    }
-
-    private func setupSubviews() {
-        tableView.dataSource = dataSource
-    }
-}
-
-internal class ItemsViewTableViewDataSource:NSObject, UITableViewDataSource {
-    public var items = [ItemData]()
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let tasksListCell = tableView.dequeueReusableCell(withIdentifier: "\(ItemTableCellView.self)", for: indexPath) as! ItemTableCellView
-        
-        let model = items[indexPath.row]
-        tasksListCell.titleLabel.text = model.title
-        tasksListCell.subtitleLabel.text = model.subtitle
-        tasksListCell.iconImageView.image = model.icon
-        
-        return tasksListCell
-    }
-    
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-}
-
 final class ItemsListViewTests: XCTestCase {
     func test_emptyState_shouldShowViewIfItemsListIsEmpty() {
         let sut = createSUT()
