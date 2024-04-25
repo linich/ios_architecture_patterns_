a4a281ee92fcb00937b6493684f8daf1c3c5d6d1
linich <linich.maksim@gmail.com>
2024-04-25 12:20:56 +0300

    Check that epty state is visible if viw has not items

--- ActivityList/ActivityListUITests/Items/ItemsListViewTests.swift
+++ ActivityList/ActivityListUITests/Items/ItemsListViewTests.swift
@@ -6,6 +6,36 @@
 //
 
 import XCTest
+import UIKit
+
+struct ItemData {
+    
+}
+class ItemsView: UIView {
+    public let emptyState = {
+        let view = UIView()
+        return view
+        
+    }()
+    
+    public var items: [ItemData] = []
+}
 
 final class ItemsListViewTests: XCTestCase {
+    func test_emptyState_shouldShowViewIfItemsListIsEmpty() {
+        let view = createSUT()
+        
+        view.items = []
+        
+        XCTAssertFalse(view.emptyState.isHidden, "Empty state view should be visible if tasksLists is empty");
+    }
+
+    // Mark: - Helpers
+    
+    fileprivate func createSUT(file: StaticString = #filePath, line: UInt = #line) -> ItemsView {
+        let view = ItemsView()
+        trackMemoryLeak(view)
+        return view
+    }
+
 }
